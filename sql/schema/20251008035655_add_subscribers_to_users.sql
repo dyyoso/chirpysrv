7f3275e7a18d5f61dbdaf4434f6eb038bfd88449
prchop <182120329+prchop@users.noreply.github.com>
2025-10-11 19:44:19 +0700

    chore(db): remove StatementBegin/End and add missing Down migration

--- sql/schema/20251008035655_add_subscribers_to_users.sql
+++ sql/schema/20251008035655_add_subscribers_to_users.sql
@@ -1,11 +1,7 @@
 -- +goose Up
--- +goose StatementBegin
-SELECT 'up SQL query';
 ALTER TABLE users
   ADD COLUMN "is_chirpy_red" BOOLEAN NOT NULL DEFAULT FALSE;
--- +goose StatementEnd
 
 -- +goose Down
--- +goose StatementBegin
-SELECT 'down SQL query';
--- +goose StatementEnd
+ALTER TABLE users
+  DROP COLUMN "is_chirpy_red";
